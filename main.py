f8433714101507665048efe2653181b8c88c2dd0
Lucas Fedrigo <contato.fedrigo@gmail.com>
2024-02-20 15:49:46 -0300

    filling forms in another test

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.python.org/")
-
-# captcha = driver.find_element(By.LINK_TEXT, "Tentar uma imagem diferente")
-# captcha.click()
-
-events_dates = driver.find_elements(By.CSS_SELECTOR, value='.event-widget time')
-events_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget .menu a")
-
-events = {}
-
-for n in range(len(events_dates)):
-    events[n] = {
-        "time": events_dates[n].text,
-        "name": events_names[n].text,
-    }
-
-print(events)
-
-# bug_link_ie = driver.find_element(By.XPATH, value="colar-aqui-o-XPATH")
+driver.get("https://secure-retreat-92358.herokuapp.com/")
 
 
-driver.quit()
+form1 = driver.find_element(By.NAME, value="fName")
+form2 = driver.find_element(By.NAME, value="lName")
+form3 = driver.find_element(By.NAME, value="email")
+button = driver.find_element(By.CLASS_NAME, value="btn")
+
+form1.send_keys("Lucas")
+form2.send_keys("Fedrigo")
+form3.send_keys("lucas.fedrigo@gmail.com")
+button.send_keys(Keys.ENTER)
+
+
