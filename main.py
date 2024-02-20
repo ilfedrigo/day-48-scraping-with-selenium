0eebab39c4c8da33eb791e546e916a6b040f812a
Lucas Fedrigo <contato.fedrigo@gmail.com>
2024-02-20 16:07:46 -0300

    Building a bot to play Click Cookie game

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://secure-retreat-92358.herokuapp.com/")
+driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
-
-form1 = driver.find_element(By.NAME, value="fName")
-form2 = driver.find_element(By.NAME, value="lName")
-form3 = driver.find_element(By.NAME, value="email")
-button = driver.find_element(By.CLASS_NAME, value="btn")
-
-form1.send_keys("Lucas")
-form2.send_keys("Fedrigo")
-form3.send_keys("lucas.fedrigo@gmail.com")
-button.send_keys(Keys.ENTER)
-
+cookie = driver.find_element(By.CSS_SELECTOR, value="#cookie")
+items = driver.find_elements(By.CSS_SELECTOR, value="#store div")
+item_ids = [item.get_attribute("id") for item in items]
 
